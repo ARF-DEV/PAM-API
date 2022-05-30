29ea6c5f1a59d0d193a671f3b5a168c2f2af62ee
PopoSnek <ariefuddinsatriadharma@gmail.com>
2022-05-30 20:12:30 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ARF-DEV/pam-api/repository"
+	"github.com/ARF-DEV/pam-api/router"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./database.db")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	initDB(db)
+
+	repo := repository.NewProductRepo(db)
+
+	mux := router.GenerateMux(repo)
+	port := os.Getenv("PORT")
+	log.Println("Server run on port 8000")
+	http.ListenAndServe(":"+port, mux)
+
+}
+
+func initDB(db *sql.DB) {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS products (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		price INTEGER NOT NULL,
+		short_description TEXT NOT NULL,
+		long_description TEXT NOT NULL
+	);`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`INSERT INTO products (name, price, short_description, long_description) VALUES 
+	('Ayam Geprek Mang Gibran', 10000, 'Ayam', 'ini long desc Ayam Geprek Mang Gibran'),
+	('Pempek om Della', 5000, 'Pempek', 'ini long desc Pempek om Della'),
+	('Wedang Jahe engkoh Eliza', 2000, 'Wedang Jahe', 'ini long desc Wedang Jahe engkoh Eliza'),
+	('Ayam Geprek Mang Arip', 10000, 'Ayam', 'ini long desc Ayam Geprek Mang Arip');
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+}
